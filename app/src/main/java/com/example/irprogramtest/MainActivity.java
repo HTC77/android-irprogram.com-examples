89d200d4545d400339042cc5af69eec282f90b57
HTC <a77sabeghi@gmail.com>
2019-03-08 21:06:11 +0330

    init radio button

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -4,13 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RadioGroup rgColor;
+    private RadioButton selectedRb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        rgColor = findViewById(R.id.rgColor);
+    }
+
+    public void onSelectBtnClicked(View v){
+        int selectedRbId = rgColor.getCheckedRadioButtonId();
+        selectedRb = findViewById(selectedRbId);
+        Toast.makeText(this, selectedRb.getText(), Toast.LENGTH_SHORT).show();
     }
 }
