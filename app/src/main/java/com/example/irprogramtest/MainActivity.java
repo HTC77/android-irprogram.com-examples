e47094e5b7f4fec88ed42a9928e00990967f39b8
HTC <a77sabeghi@gmail.com>
2019-03-08 21:33:56 +0330

    toggle image resource

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -4,27 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private RadioGroup rgColor;
-    private RadioButton selectedRb;
+    private ImageView img1;
+    private boolean picToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        picToggle = false;
         findViews();
     }
 
     private void findViews() {
-        rgColor = findViewById(R.id.rgColor);
+        img1 = findViewById(R.id.img1);
     }
 
-    public void onSelectBtnClicked(View v){
-        int selectedRbId = rgColor.getCheckedRadioButtonId();
-        selectedRb = findViewById(selectedRbId);
-        Toast.makeText(this, selectedRb.getText(), Toast.LENGTH_SHORT).show();
+    public void imgBtnClicked(View v){
+        img1.setImageResource(picToggle ? R.mipmap.m_pic1 : 0);
+        picToggle = picToggle ? false : true;
     }
 }
