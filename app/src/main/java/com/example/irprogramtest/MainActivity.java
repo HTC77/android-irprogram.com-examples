17de25a2115a878d24fe996665870c2e0049728a
HTC <a77sabeghi@gmail.com>
2019-03-08 19:45:58 +0330

    alert dialog

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.irprogramtest;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,17 +10,26 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etUserName = findViewById(R.id.etUsername);
     }
 
-    public void nextBtnClicked(View v){
-        Intent intent = new Intent(MainActivity.this,NewActivity.class);
-        intent.putExtra("uName",etUserName.getText().toString());
-        startActivity(intent);
+    public void onExitClicked(View v){
+        AlertDialog.Builder alertBuiler = new AlertDialog.Builder(this);
+        alertBuiler.setCancelable(false);
+        alertBuiler.setMessage("آیا مطمئن هستید که می خواهید خارج شوید؟");
+        alertBuiler.setPositiveButton("بله", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        }).setNegativeButton("خیر", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        }).setTitle("خروج از برنامه").show();
     }
 }
