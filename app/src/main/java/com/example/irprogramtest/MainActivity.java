4adc9836cedbcae44762effa5eaf0b5d8d8af54e
HTC <a77sabeghi@gmail.com>
2019-03-08 19:16:48 +0330

    next Activity

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -4,17 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etUserName = findViewById(R.id.etUsername);
     }
 
     public void nextBtnClicked(View v){
-        startActivity(new Intent(MainActivity.this,NewActivity.class));
+        Intent intent = new Intent(MainActivity.this,NewActivity.class);
+        intent.putExtra("uName",etUserName.getText().toString());
+        startActivity(intent);
     }
 }
