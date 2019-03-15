f2b59ff1dcd7f7880cf3dcdf4c9099591304c9de
HTC <a77sabeghi@gmail.com>
2019-03-15 20:19:49 +0330

    listView

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.irprogramtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvLink;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvLink =findViewById(R.id.tvLink);
-        tvLink.setClickable(true);
-        tvLink.setMovementMethod(LinkMovementMethod.getInstance());
-        String htmlText = "<a href='http://irprogram.com'> irProgram </a>";
-        tvLink.setText(Html.fromHtml(htmlText));
+        listView =findViewById(R.id.listview);
+
+        String[] data = new String[]{"Ubuntu", "Arch", "openSUSE", "kali"};
+        ArrayList<String> myArray = new ArrayList<String>();
+        for (int i = 0; i < data.length; i++)
+            myArray.add(data[i]);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,myArray);
+        listView.setAdapter(adapter);
     }
 }
