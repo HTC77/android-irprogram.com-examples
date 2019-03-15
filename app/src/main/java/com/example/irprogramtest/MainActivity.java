11441b5fd9deb4583fd070cbf6ca8e9c9bf21ba5
HTC <a77sabeghi@gmail.com>
2019-03-15 20:05:47 +0330

    link

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.irprogramtest;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvLink =findViewById(R.id.tvLink);
+        tvLink.setClickable(true);
+        tvLink.setMovementMethod(LinkMovementMethod.getInstance());
+        String htmlText = "<a href='http://irprogram.com'> irProgram </a>";
+        tvLink.setText(Html.fromHtml(htmlText));
     }
 }
