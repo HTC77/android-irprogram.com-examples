7ac1504ebd748942d559cf104baf7d95a45dfcc2
HTC <a77sabeghi@gmail.com>
2019-03-15 22:25:33 +0330

    init Clipboard

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.irprogramtest;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etCopy, etPaste;
+    private ClipboardManager clipboardManager;
+    private ClipData clipData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etCopy = findViewById(R.id.etCopy);
+        etPaste =findViewById(R.id.editPaste);
+
+        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+    }
+
+    public void onBtnCopyClicked(View v){
+        String text = etCopy.getText().toString();
+        clipData = ClipData.newPlainText("matn",text);
+        clipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(this, "Text copied", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onBtnPasteClicked(View v){
+        ClipData mClipData = clipboardManager.getPrimaryClip();
+        String text = mClipData.getItemAt(0).getText().toString();
+        etPaste.setText(text);
+        Toast.makeText(this, "Pasted", Toast.LENGTH_SHORT).show();
     }
 }
