ec3bfe763522e645995054ac5e7a610ac33ee5cf
HTC <a77sabeghi@gmail.com>
2019-03-15 23:49:25 +0330

    init Camera

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.irprogramtest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int REQUEST_IMAGE_CAPTURE = 27;
+    ImageView myImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myImageView = findViewById(R.id.imageView);
+        if(!hasCamera()){
+            Button btnImage = findViewById(R.id.btnTakePhoto);
+            btnImage.setText("Can not find camera");
+            btnImage.setEnabled(false);
+        }
+    }
+
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    public void onBtnTakeImageClicked(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle bundle = data.getExtras();
+            Bitmap photo = (Bitmap) bundle.get("data");
+            myImageView.setImageBitmap(photo);
+        }else {
+            Toast.makeText(this,
+                    "An error", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
