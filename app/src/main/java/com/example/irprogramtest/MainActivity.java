1e01ff057b28e589f7d112e06c27c577e01810e0
HTC <a77sabeghi@gmail.com>
2019-03-20 13:46:33 +0330

    customize style and colors

--- app/src/main/java/com/example/irprogramtest/MainActivity.java
+++ app/src/main/java/com/example/irprogramtest/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.irprogramtest;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
