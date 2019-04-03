dae16348e123c04461e4ebc01943c9b0141a896b
HTC <a77sabeghi@gmail.com>
2019-04-04 00:15:37 +0430

    books index

---
+++ app/src/main/java/com/example/irprogramtest/DatabaseHandler.java
@@ -0,0 +1,109 @@
+package com.example.irprogramtest;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private Context mContext;
+    private static String DB_PATH;
+    private static int DB_VERSION = 1;
+    private final static String DB_NAME = "ebook_db.db";
+    private final static String TBL_BOOKS = "books";
+    private final static String TAG = "HTC_EXC";
+    private final static String TBL_SETTINGS = "settings";
+    private SQLiteDatabase db;
+
+    public DatabaseHandler(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        mContext = context;
+        DB_PATH = mContext.getCacheDir().getPath() + "/" + DB_NAME;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private boolean dbExists(){
+        File f = new File(DB_PATH);
+        return f.exists() ? true : false;
+    }
+
+    private boolean copyDb(){
+        try {
+            FileOutputStream outputStream = new FileOutputStream(DB_PATH);
+            InputStream inputStream = mContext.getAssets().open(DB_NAME);
+            byte[] buffer = new byte[1024];
+            int ch;
+            while ((ch = inputStream.read(buffer)) >0)
+                outputStream.write(buffer,0,ch);
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+            return true;
+        }catch (Exception e){
+            Log.e(TAG, "copyDb DatabaseHandler: ", e);
+            return false;
+        }
+    }
+
+    public void open(){
+        if (dbExists()){
+            try {
+                File temp = new File(DB_PATH);
+                db = SQLiteDatabase.openDatabase(
+                        temp.getAbsolutePath(),
+                        null,
+                        SQLiteDatabase.OPEN_READWRITE
+                );
+            }catch (Exception e){
+                Log.e(TAG, "open DatabaseHandler: ", e);
+            }
+        }else {
+            if (copyDb())
+                open();
+        }
+    }
+
+    @Override
+    public synchronized void close() {
+        db.close();
+    }
+
+    public List<HashMap<String,Object>> getBooks(){
+        Cursor result = db.rawQuery("SELECT * FROM "+TBL_BOOKS,
+                null);
+        List<HashMap<String,Object>> allData = new ArrayList<>();
+        while (result.moveToNext()){
+            HashMap<String,Object> temp = new HashMap<>();
+            temp.put("id",result.getString(0));
+            temp.put("title",result.getString(1));
+            temp.put("author",result.getString(3));
+            temp.put("fav_flag",result.getString(5).equals("1")?
+                    R.drawable.is_favorite:
+                    R.drawable.not_favorite);
+            temp.put("see_flag",result.getString(6).equals("1")?
+                    R.drawable.see:
+                    R.drawable.not_see);
+
+            allData.add(temp);
+        }
+        return allData;
+    }
+}
--- app/src/main/java/com/example/irprogramtest/IndexActivity.java
+++ app/src/main/java/com/example/irprogramtest/IndexActivity.java
@@ -1,16 +1,56 @@
 package com.example.irprogramtest;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class IndexActivity extends AppCompatActivity {
-
+    private ListView lvBooksIndex;
+    private DatabaseHandler db;
+    private List<HashMap<String,Object>> books;
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        lvBooksIndex = findViewById(R.id.lvBooksIndex);
 
+        db = new DatabaseHandler(this);
+
+        db.open();
+        books = db.getBooks();
+        String[] from = {"title", "author", "fav_flag", "see_flag"};
+        int[] to = {R.id.tvTitleIndex, R.id.tvAuthorIndex,
+                R.id.imgSetFavIndex,R.id.imgSetSeeIndex};
+        SimpleAdapter adapter = new SimpleAdapter(
+                getBaseContext(), books,
+                R.layout.row_index_book,
+                from,to);
+        lvBooksIndex.setAdapter(adapter);
+        db.close();
+
+        lvBooksIndex.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getBaseContext(),BookContentActivity.class);
+                String mId = books.get(position).get("id").toString();
+                intent.putExtra("id",mId);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onBtnBackClicked(View v){
+        finish();
     }
 }
