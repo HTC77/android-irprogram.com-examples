2cd6067694038adca5c786af7bb5835b7f24304e
HTC <a77sabeghi@gmail.com>
2019-05-07 23:48:21 +0430

    SplashScreen

--- app/src/main/java/com/example/irprogramtest/SplashScreen.java
+++ app/src/main/java/com/example/irprogramtest/SplashScreen.java
@@ -1,5 +1,8 @@
 package com.example.irprogramtest;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -13,5 +16,29 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         mWebView =findViewById(R.id.mWebView);
+        String tx = "<center>" +
+                "<img src='file:///android_asset/wait.gif' alt='picture' />" +
+                "</center>";
+
+        mWebView.loadDataWithBaseURL(
+                null, tx , "text/html; charset='utf-8'" , null , null
+        );
+
+        mWebView.setBackgroundColor( Color.TRANSPARENT );
+
+        new Handler().postDelayed(
+            new Runnable() {
+                @Override
+                public void run()
+                {
+                    Intent i = new Intent(SplashScreen.this , MainActivity.class);
+
+                    SplashScreen.this.startActivity(i);
+                    /* animation */
+
+                    SplashScreen.this.finish();
+                }
+            } , SPLASH_TIME
+        );
     }
 }
